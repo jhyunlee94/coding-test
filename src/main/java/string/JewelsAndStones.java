462cd2e43fe8990a45034a38761761347ba8375d
jhyun <jkwhd12@gmail.com>
2024-03-06 08:40:03 +0900

    String : JewelsAndStones.java

--- src/main/java/string/JewelsAndStones.java
+++ src/main/java/string/JewelsAndStones.java
@@ -1,18 +1,35 @@
 package string;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class JewelsAndStones {
 	public static void main(String[] args) {
 		JewelsAndStones jewelsAndStones = new JewelsAndStones();
-		String jewels = "aA";
-		String stones = "aAAbbbb";
+		// String jewels = "aA";
+		// String stones = "aAAbbbb";
+		String jewels = "z";
+		String stones = "ZZ";
+		// 3 이 나와야함
 		System.out.println(jewelsAndStones.solution(jewels, stones));
 	}
 
 	private int solution(String jewels, String stones) {
+		List<Character> list = new ArrayList<>();
+		char[] jewelsArr = jewels.toCharArray();
+		for (char c : jewelsArr) {
+			list.add(c);
+		}
 
-		return 0;
+		int count = 0;
+		char[] stonesArr = stones.toCharArray();
+		for (char c : stonesArr) {
+			if (list.contains(c)) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
