7285d48508da62a42dcaf49bd1c69e5328bc9a97
jhyun <jkwhd12@gmail.com>
2024-03-07 08:03:12 +0900

    String : JewelsAndStones.java 수정

--- src/main/java/string/JewelsAndStones.java
+++ src/main/java/string/JewelsAndStones.java
@@ -8,28 +8,29 @@ import java.util.Set;
 public class JewelsAndStones {
 	public static void main(String[] args) {
 		JewelsAndStones jewelsAndStones = new JewelsAndStones();
-		// String jewels = "aA";
-		// String stones = "aAAbbbb";
-		String jewels = "z";
-		String stones = "ZZ";
+		String jewels = "aA";
+		String stones = "aAAbbbb";
+		// String jewels = "z";
+		// String stones = "ZZ";
 		// 3 이 나와야함
+		// 0 이 나와야함
 		System.out.println(jewelsAndStones.solution(jewels, stones));
 	}
 
 	private int solution(String jewels, String stones) {
-		List<Character> list = new ArrayList<>();
-		char[] jewelsArr = jewels.toCharArray();
-		for (char c : jewelsArr) {
-			list.add(c);
+		Set<Character> set = new HashSet<>();
+
+		for (char c : jewels.toCharArray()) {
+			set.add(c);
 		}
 
 		int count = 0;
-		char[] stonesArr = stones.toCharArray();
-		for (char c : stonesArr) {
-			if (list.contains(c)) {
+		for(char c : stones.toCharArray()) {
+			if (set.contains(c)) {
 				count++;
 			}
 		}
 		return count;
+
 	}
 }
