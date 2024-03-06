de92d0bc04b77b31c706f5b121a239d7e6b310f7
jhyun <jkwhd12@gmail.com>
2024-03-07 08:08:34 +0900

    String : LicenseKeyFormatting.java 수정

--- src/main/java/string/LicenseKeyFormatting.java
+++ src/main/java/string/LicenseKeyFormatting.java
@@ -5,24 +5,22 @@ public class LicenseKeyFormatting {
 
 	public static void main(String[] args) {
 		LicenseKeyFormatting licenseKeyFormatting = new LicenseKeyFormatting();
-		String s = "8F3Z-2E-9-w";
-		int k = 4;
-		// String s = "8-5g-3-J";
-		// int k = 2;
+		// String s = "8F3Z-2E-9-w";
+		// int k = 4;
+		String s = "8-5g-3-J";
+		int k = 2;
 		// result = 8F3Z-2E9W, 8-5G-3J
 		System.out.println(licenseKeyFormatting.solution(s, k));
 	}
 
 	private String solution(String s, int k) {
-		s = s.replace("-","").toUpperCase();
-		// System.out.println(s);
+		s = s.toUpperCase().replace("-", "");
 		StringBuilder sb = new StringBuilder(s);
 
-		int len = sb.length();
-		for (int i = k; i < len; i=i+k) {
-			sb.insert(len-i, "-");
-		}
+		for (int i = k; i < s.length(); i = i + k) {
+			sb.insert(s.length() - i, "-");
 
+		}
 		return sb.toString();
 	}
 
